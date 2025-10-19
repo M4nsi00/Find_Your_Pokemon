9cabb25bb864a3b5c71b84b0e3671132ae06c536
Sebastian Mansilla <119966292+M4nsi00@users.noreply.github.com>
2025-10-19 17:50:19 -0600

    Actualización Main, por historial

--- Find_Your_Pokemon.cpp
+++ Find_Your_Pokemon.cpp
@@ -4,6 +4,7 @@
 #include "CargaDatos.h"
 #include "Filtros.h"
 #include "Ordenamiento.h"
+#include "Historial.h"
 
 void mostrarPokemon(const std::vector<Pokemon>& lista) {
     for (const auto& p : lista) {
@@ -19,6 +20,7 @@ void mostrarPokemon(const std::vector<Pokemon>& lista) {
 
 int main() {
     std::vector<Pokemon> pokedex = cargarPokemon("pokemon.csv");
+    Historial historial;
     if (pokedex.empty()) {
         std::cerr << "No se cargaron datos.\n";
         return 1;
@@ -31,36 +33,49 @@ int main() {
         std::cout << "2. Filtrar por tipo y ordenar por ataque especial\n";
         std::cout << "3. Filtrar por generacion y ordenar por ataque\n";
         std::cout << "4. Filtrar por generacion y ordenar por ataque especial\n";
+        std::cout << "5. Deshacer ultimo filtro\n";
         std::cout << "0. Salir\n";
         std::cout << "Opcion: ";
         std::cin >> opcion;
+        
+        if (opcion >= 1 && opcion <= 4){
+            if (opcion == 1 || opcion == 2) {
+                int tipo; 
+                std::cout << "Ingrese tipo (1-18): "; 
+                std::cin >> tipo;
+                historial.reigstrarFiltro(pokedex);
 
-        if (opcion == 1 || opcion == 2) {
-            int tipo; 
-            std::cout << "Ingrese tipo (1-18): "; 
-            std::cin >> tipo;
-            auto filtrados = filtrarPorTipo(pokedex, tipo);
+                pokedex = filtrarPorTipo(pokedex, tipo);
 
-            if (opcion == 1)
-                mergeSort(filtrados, 0, filtrados.size() - 1, compararPorAtaqueAsc);
+                if (opcion == 1){
+                    mergeSort(pokedex, 0, pokedex.size() - 1, compararPorAtaqueAsc);
+                }else{
+                    mergeSort(pokedex, 0, pokedex.size() - 1, compararPorSpAtkAsc);
+                }
+            }
+            else if (opcion == 3 || opcion == 4) {
+                int gen; 
+                std::cout << "Ingrese generacion (1-7): "; 
+                std::cin >> gen;
+                historial.reigstrarFiltro(pokedex);
+                
+                pokedex = filtrarPorGeneracion(pokedex, gen);
+
+                if (opcion == 3){
+                    mergeSort(pokedex, 0, pokedex.size() - 1, compararPorAtaqueAsc);
+                }else{
+                    mergeSort(pokedex, 0, pokedex.size() - 1, compararPorSpAtkAsc);
+                }
+        } 
+             mostrarPokemon(pokedex);
+    }else if (opcion == 5){
+            if (!historial.deshacerUltimoCambio(pokedex))
+                std::cout << "No hay filtros previos para deshacer.\n";
             else
-                mergeSort(filtrados, 0, filtrados.size() - 1, compararPorSpAtkAsc);
-
-            mostrarPokemon(filtrados);
+                mostrarPokemon(pokedex);
         }
-        else if (opcion == 3 || opcion == 4) {
-            int gen; 
-            std::cout << "Ingrese generacion (1-7): "; 
-            std::cin >> gen;
-            auto filtrados = filtrarPorGeneracion(pokedex, gen);
-
-            if (opcion == 3)
-                mergeSort(filtrados, 0, filtrados.size() - 1, compararPorAtaqueAsc);
-            else
-                mergeSort(filtrados, 0, filtrados.size() - 1, compararPorSpAtkAsc);
-
-            mostrarPokemon(filtrados);
-        }
+        
+        cout << "Historial de estados guardados: " << historial.tamanio() << endl;
 
     } while (opcion != 0);
 
